54011941c69c78aee162f484911aab3a77f342c2
sentinelcoredev <core.dev.op@gmail.com>
2026-08-05 18:50:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,6 +20,6 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+  export const db = getFirestore(app);
 
   console.log("Firebase connected successfully!");
